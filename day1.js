de70e166ffdcd0bc58bb7d03194e41b29c19c937
Big Boss <oyebamijiusmanayomide@gmail.com>
2026-04-20 22:55:37 +0100

    Day 2 - arrays, forEach, callbacks

--- day1.js
+++ day1.js
@@ -39,3 +39,20 @@ let bigNumber = (num1, num2) => {
     } 
 }
 console.log(bigNumber(4, 2));
+
+//Task 3
+let numbers = [4, 9, 2, 17, 3, 8, 1];
+
+function findLargest(arr) {
+    let largest = arr[0]; 
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i]; 
+        }
+    }
+    
+    return largest;
+}
+
+console.log(findLargest(numbers));

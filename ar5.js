6bd4df41b45160c14667e83cd57000c26b2837c3
Big Boss <oyebamijiusmanayomide@gmail.com>
2026-04-22 23:50:13 +0100

    string lengths and retrieving numbers of character

---
+++ ar5.js
@@ -0,0 +1,3 @@
+//string lengths and retrieving numbers of character
+let tweetCount = prompt("Write here!");
+alert("You have written " + tweetCount.length ". You have " + 200 - tweet.length " left");

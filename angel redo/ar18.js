71a4faa1ccf77eb00d29ef83e3666cc7dba45e2f
Big Boss <oyebamijiusmanayomide@gmail.com>
2026-04-25 00:47:47 +0100

    Fibonacci Sequence

---
+++ angel redo/ar18.js
@@ -0,0 +1,21 @@
+function fibonacci(n){
+    let sequence = [];
+    let a = 0;
+    let b = 1;
+    for(let i = 0; i < n; i++){
+        if(i == 0) {
+            sequence.push(a);
+        }
+        else if(i == 1){
+            sequence.push(b);
+        }
+        else{
+            let next = a + b;
+            sequence.push(next);c
+            a = b;
+            b = next;
+        }
+    }
+    return sequence;
+}
+console.log(fibonacci(50));

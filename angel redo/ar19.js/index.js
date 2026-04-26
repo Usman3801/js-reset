4d8ae0201b183e05441f1af92a97c9eb6418649e
Big Boss <oyebamijiusmanayomide@gmail.com>
2026-04-26 01:04:32 +0100

    Angela Yu's DOM module completed

---
+++ angel redo/ar19.js/index.js
@@ -0,0 +1,5 @@
+//alert("Hello!");
+document.querySelector("h1").innerHTML = "Good Bye!";
+document.querySelector("#list a").style.color = "red";
+document.querySelector("button").style.backgroundColor = "yellow";
+document.querySelector("a").classList.add("huge");
